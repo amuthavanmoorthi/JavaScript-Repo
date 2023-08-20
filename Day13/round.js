b64b17057a0b43198761239c5c57f50687d68b4c
amuthavanmoorthi <amuthavanmoorthi@gmail.com>
2023-08-20 07:55:22 +0530

    Arrow functions

--- Day13/round.js
+++ Day13/round.js
@@ -1,6 +1,9 @@
-// Function Declaration 
+'use strict';
 
-document.write(age(20));
+
+// Function Declaration S
+
+// document.write(age(20));
 
 function age(yourage){
     return 2023- yourage;
@@ -13,6 +16,21 @@ const age1 = function age(yourage){
     return 2023- yourage;
 }
 
-document.write(age1(24));
+// document.write(age1(24));
 
 // The Difference between these is we invoke the function before declara the function declaration 
+
+
+// Arrow Function 
+
+const age3 = birth => birth - 2023;
+// document.write(age3(20));
+
+const age4 = (birth,name) => {
+   console.log( birth - 2023);
+   const goal = `You are ${name} win!`;
+   return goal;
+};
+// document.write(age4(20, 'amudhavan'));
+// document.write(age4(25, 'prasanth hari'));
+// document.write(age4(27, 'aravind,mani, kamesh'));
