03a10bc573b48584ba6512e8de437a640594acbd
amuthavanmoorthi <amuthavanmoorthi@gmail.com>
2023-08-20 07:22:33 +0530

    function expression

---
+++ Day13/round.js
@@ -0,0 +1,18 @@
+// Function Declaration 
+
+document.write(age(20));
+
+function age(yourage){
+    return 2023- yourage;
+}
+
+
+// function exprression 
+
+const age1 = function age(yourage){
+    return 2023- yourage;
+}
+
+document.write(age1(24));
+
+// The Difference between these is we invoke the function before declara the function declaration 
