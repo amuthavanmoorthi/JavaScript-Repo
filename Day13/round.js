e81aeacb2bcb9260cbd5599dea91df7fcf3a2c6e
amuthavanmoorthi <amuthavanmoorthi@gmail.com>
2023-08-21 16:03:24 +0530

    Inner Function

--- Day13/round.js
+++ Day13/round.js
@@ -34,3 +34,33 @@ const age4 = (birth,name) => {
 // document.write(age4(20, 'amudhavan'));
 // document.write(age4(25, 'prasanth hari'));
 // document.write(age4(27, 'aravind,mani, kamesh'));
+
+
+
+// Inner function 
+// const cutPieces = function (fruit){
+//     return fruit*4;
+// }
+
+// const fruitProcessor = function (apples, oranges){
+//     const applePieces = cutPieces(apples);
+//     const orangePieces = cutPieces(oranges);
+
+//     const juice =`juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2,3));
+
+// Expereience 
+
+// let a = (me) =>{
+//     console.log(me*4);
+// }
+
+// let b = (you) => {
+//     let c = a(you);
+//     console.log(c);
+// }
+// console.log(b(1));
+
+
