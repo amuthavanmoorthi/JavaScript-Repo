104de8d07cfdb4675f8b95c83c02fe31c94df942
amuthavanmoorthi <amuthavanmoorthi@gmail.com>
2023-08-22 07:25:11 +0530

    break and continue.

---
+++ Day15/index.js
@@ -0,0 +1,33 @@
+// const years = [111,222,333,444];
+// const age =[];
+
+// for (let i=0; i<years.length; i++){
+//     // const yeah =(`This is years ${years[i]}`);
+//     // console.log(yeah);
+// age.push(years[i]);
+    
+// }
+// console.log("This is age",age);
+
+const me=[
+     'amudhavan',
+    20,
+    'trichy',
+     100
+]
+
+console.log("---ONLY STRING---");
+
+for (let i = 0; i<me.length; i++){
+    if(typeof me[i] !== 'string') continue; //it will print only a string value.
+
+    console.log(me[i], typeof me[i]);
+}
+
+console.log("---ONLY NUMBER---");
+
+for (let i = 0; i<me.length; i++){
+    if(typeof me[i] === 'number') break; //It will break the loop when the number will come.
+
+    console.log(me[i], typeof me[i]);
+}
