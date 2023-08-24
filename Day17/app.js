088b364bbbe47fceae4ebf3a5e2e0eeb6c540406
amuthavanmoorthi <amuthavanmoorthi@gmail.com>
2023-08-24 14:06:08 +0530

    object calling

---
+++ Day17/app.js
@@ -0,0 +1,38 @@
+// let pasta ={
+//     name : "Tomoto Pasta",
+//     imgSrc :"https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/recipe-img.png",
+//     heading : "Ingredients",
+//     skills : ['pasta','oil','Onions','Salt','Tomoto pasta sause', 'Cheese']
+// };
+
+
+// document.getElementById("cardTitle").textContent=pasta.name;
+// document.getElementById('imgId').src=pasta.imgSrc;
+// document.getElementById('ingredients').textContent=pasta.heading;
+
+// for (let things of pasta.skills){
+// let ul = document.getElementById('ulid')
+// let list = document.createElement('li')
+// list.textContent=things;
+// ul.appendChild(list);
+// };
+
+
+// Object calling and appennding the ul li  
+// const food = {
+//     head : "Tomoto Pasta",
+//     imgSrc : "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/recipe-img.png",
+//     innerHead : "Ingredients",
+//     receipe : ['pasta','Onions','massla','tomoto']
+// }
+
+// document.getElementById('cardTitle').textContent=food.head;
+// document.getElementById('imgId').src=food.imgSrc;
+// document.getElementById('ingredients').textContent=food.innerHead;
+
+// for (let things of food.receipe){
+// let hello = document.getElementById('ulid')
+// let lists = document.createElement('li');
+// hello.appendChild(lists);
+// lists.textContent = things;
+// }
